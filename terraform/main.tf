81a4a5cbf50e94c4e318ac384c1089336c6b54e6
nathanielkay11-tech <nathanielkay11@gmail.com>
2026-04-08 17:15:07 +0200

    feat: add terraform provider and version configuration

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+# --- Terraform Configuration ---
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+# --- AWS Provider ---
+
+provider "aws" {
+  region = var.region
+}
